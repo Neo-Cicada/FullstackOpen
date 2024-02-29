61abe1c3e1600e0b6f7efd5f7de12a1d68d96c9a
Neo <lordneobarnachea@gmail.com>
2024-02-29 21:55:20 +0800

    adjust base on the backend server

--- part2/phonebook/src/service/phoneBook.js
+++ part2/phonebook/src/service/phoneBook.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "https://phonebookbackend-7tqt.onrender.com/api/persons"
 
 const postItem = (person) =>{
     return(
